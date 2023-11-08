730edfd345aeffc7272b6e41774fefa4cdf4619e
busrabeyazz <busrayalmanbeyaz@gmail.com>
2023-11-08 10:27:34 +0300

    counter
    8 kasim counter örneği

---
+++ ReactCourse_Counter/src/Counter.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"; // Import useState from react
+
+function Counter() {
+    const [count, setCount] = useState(0); // Declare count and setCount using the useState hook
+
+    const increment = () => {
+        if (count < 10){
+        setCount(count + 1)}; // Increment count
+    };
+    const decrement = () => {
+        if (count > 0) {
+        setCount(count - 1)}; // Decrement count
+    };
+
+    // Now, return the JSX for your component
+    return (
+        <div className="container mt-4">
+            <div className="Sayac">
+            <span>Count: {count}</span>
+            <br></br>
+                <button type="button" class="btn btn-primary" onClick={increment}>Artır</button>
+                <button type="button" class="btn btn-danger" onClick={decrement}>Azalt</button> 
+            </div>
+        </div>
+    );
+}
+
+export default Counter; // Export the Counter component
