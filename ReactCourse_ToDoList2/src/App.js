30cdf2b961f1f83be657fb3e45d777747fc7cc9c
busrabeyazz <busrayalmanbeyaz@gmail.com>
2023-11-08 15:05:12 +0300

    todo list devam
    burada farklı olarak başlık ve açıklama ekliyoruz, aynı buton hem ekleyip hem siliyor

---
+++ ReactCourse_ToDoList2/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Counter from './Counter';
+import ToDoList from './ToDoList';
+function App() {
+  return (
+    <div className="App">
+      <div class="container text-center">
+  <div class="row">
+    <div class="col">
+    <Counter/>
+    </div>
+    <div class="col">
+    <ToDoList/>
+    </div>
+    <div class="col">
+      -
+    </div>
+  </div>
+</div>
+      
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactCourse_ToDoList2/src/ToDoList.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const addTask = () => {
+    // Check to avoid adding an empty task
+    if (title && description) {
+      // Add the new task with title and description to the current task list
+      const newTask = { title, description };
+      setTasks([...tasks, newTask]);
+      // Clear the input fields
+      setTitle('');
+      setDescription('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h3>To-Do List</h3>
+      <div style={{ display: 'flex', marginBottom: '10px' }}>
+        <input 
+          type="text"
+          className="form-control"
+          placeholder="Title" // Added a placeholder for better UX
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          style={{ marginRight: '5px' }}
+        />
+        <input 
+          type="text"
+          className="form-control"
+          placeholder="Description" // Added a placeholder for better UX
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          style={{ marginRight: '5px' }}
+        />
+        <button 
+          onClick={addTask} 
+          type="button" 
+          className="btn btn-outline-success"
+        >
+          Ekle
+        </button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <strong>{task.title}</strong>: {task.description} 
+            <button 
+              type="button" 
+              className="btn btn-outline-danger"
+              onClick={() => setTasks(tasks.filter((_, i) => i !== index))}
+            >
+              Sil
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
